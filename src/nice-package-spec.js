91282f6b58156951de59cd7461210e5c9e1787f9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-04-07 13:22:02 -0400

    chore: initial scaffolded code

---
+++ src/nice-package-spec.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/* eslint-env mocha */
+const nicePackage = require('.')
+
+describe('@bahmutov/nice-package', () => {
+  it('write this test', () => {
+    console.assert(nicePackage, 'should export something')
+  })
+})
